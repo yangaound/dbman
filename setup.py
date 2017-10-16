572c3343aa63d6ae93f36a968be272dca70179f3
yangaound <yangaound@users.noreply.github.com>
2017-10-16 11:52:52 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = '1.0.0'
+
+package_dir = 'dbman'
+
+setup(name='dbman',
+
+    version=version,
+
+    description='I/O Interface to Pure Python Database Driver',
+
+    packages=[package_dir, ],
+)
